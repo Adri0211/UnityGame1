9e0be0227ab7d7fb0b957a29e0a634d6bbe6ccc0
Adri0211 <79997824+Adri0211@users.noreply.github.com>
2022-05-12 23:03:07 +0200

    Basic Movement (Innacurate)

---
+++ MarioGalaxy/Assets/Script/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject[] planets;
+    public GameObject camara;
+    public int speed;
+    public int jumpH;
+
+    private Rigidbody rb;
+    private bool onGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+
+        transform.Translate(x, 0.0f, z, camara.transform);
+
+        Physics.gravity = planets[0].transform.position - transform.position;
+        transform.rotation = Quaternion.FromToRotation(transform.up, -Physics.gravity) * transform.rotation;
+
+        if (onGround)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rb.AddForce(transform.up*jumpH*100);
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            onGround = false;
+        }
+    }
+}
